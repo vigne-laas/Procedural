0c577717d32398ad97d2f4ad732cc7221ce4b98a
avigne <adrien.vigne@laas.fr>
2023-01-18 11:04:02 +0100

    create Facts

---
+++ include/procedural/core/Facts.h
@@ -0,0 +1,17 @@
+//
+// Created by avigne on 18/01/23.
+//
+
+#ifndef PROCEDURAL_FACTS_H
+#define PROCEDURAL_FACTS_H
+
+namespace procedural {
+
+class Facts
+{
+
+};
+
+} // procedural
+
+#endif //PROCEDURAL_FACTS_H
---
+++ src/core/Facts.cpp
@@ -0,0 +1,8 @@
+//
+// Created by avigne on 18/01/23.
+//
+
+#include "procedural/core/Facts.h"
+
+namespace procedural {
+} // procedural
