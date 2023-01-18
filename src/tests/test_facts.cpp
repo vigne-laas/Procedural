44e2ed5c26e81cb1aef46d235ae43252f99a36c2
avigne <adrien.vigne@laas.fr>
2023-01-18 11:07:59 +0100

    create test_facts.cpp

---
+++ src/tests/test_facts.cpp
@@ -0,0 +1,5 @@
+//
+// Created by avigne on 18/01/23.
+//
+
+#include "test_facts.h"
---
+++ src/tests/test_facts.h
@@ -0,0 +1,15 @@
+//
+// Created by avigne on 18/01/23.
+//
+
+#ifndef PROCEDURAL_TEST_FACTS_H
+#define PROCEDURAL_TEST_FACTS_H
+
+
+class test_facts
+{
+
+};
+
+
+#endif //PROCEDURAL_TEST_FACTS_H
