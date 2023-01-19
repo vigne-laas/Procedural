de70c19cfbe711da0c5d8bdd7b4ab268959a73ff
avigne <adrien.vigne@laas.fr>
2023-01-19 17:30:57 +0100

    [feeder] add parse function with regex

---
+++ include/procedural/feeder/Feeder.h
@@ -0,0 +1,24 @@
+#ifndef PROCEDURAL_FEEDER_H
+#define PROCEDURAL_FEEDER_H
+
+#include <regex>
+#include <string>
+#include <vector>
+#include "procedural/core/Fact.h"
+
+namespace procedural {
+
+class Feeder
+{
+public:
+    Feeder();
+    Fact* parse(const std::string& fact);
+private:
+    std::regex const pattern;
+
+
+};
+
+} // procedural
+
+#endif //PROCEDURAL_FEEDER_H
---
+++ src/feeder/Feeder.cpp
@@ -0,0 +1,39 @@
+//
+// Created by avigne on 18/01/23.
+//
+
+#include "procedural/feeder/Feeder.h"
+#include <iostream>
+
+namespace procedural {
+
+Feeder::Feeder() : pattern("\\[([^\\]]+)\\]([^|]+)\\|([^|]+)\\|(.+)"){
+
+}
+
+
+procedural::Fact* Feeder::parse(const std::string& fact)
+{
+    std::smatch matches;
+    if (std::regex_search(fact, matches, pattern))
+    {
+        std::cout << "match ok " << std::endl;
+//        for (auto i = 0; i < matches.size(); ++i)
+//        {
+//            std::cout << i << ": '" << matches[i].str() << "'" << std::endl;
+//        }
+//        if (matches.size() == 5)
+//        {
+//
+//        }
+        if (matches[1].str() == "DEL")
+            return new Fact(false, matches[2], matches[3], matches[4]);
+        else
+            return new Fact(true, matches[2], matches[3], matches[4]);
+
+    }
+    return nullptr;
+}
+
+
+} // procedural
