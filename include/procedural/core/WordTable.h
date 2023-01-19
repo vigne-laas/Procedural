e09f4494e0dc527e3ce07d37d2312eb7971899d3
avigne <adrien.vigne@laas.fr>
2023-01-19 17:30:32 +0100

    [core] WordTable add [] operator and getter and get_const

--- include/procedural/core/WordTable.h
+++ include/procedural/core/WordTable.h
@@ -1,9 +1,9 @@
 #ifndef PROCEDURAL_WORDTABLE_H
 #define PROCEDURAL_WORDTABLE_H
 
-#include <vector>
 #include <string>
 #include <unordered_set>
+#include <vector>
 
 namespace procedural {
 
@@ -11,15 +11,21 @@ class WordTable
 {
 public:
     WordTable();
+    ~WordTable() = default;
+
     void printSize();
     void printAll();
+
     void add(const std::string& word);
-    void add(std::unordered_set<std::string> list); // pb avec const std::string &  et const set
-    std::string& get(uint32_t index);
+    void add(const std::unordered_set<std::string>& list);
+
+    const std::string& get(uint32_t index);
     uint32_t get(const std::string& word);
-    const std::string& operator[](uint32_t index) const;
-    std::string& operator[](uint32_t index);
-    uint32_t operator[](std::string word);
+
+    uint32_t get_const(const std::string& word) const;
+
+    const std::string& operator[](uint32_t index);
+    uint32_t operator[](const std::string& word);
 
 private:
     std::vector<std::string> table_;
--- src/core/WordTable.cpp
+++ src/core/WordTable.cpp
@@ -4,7 +4,7 @@
 
 namespace procedural {
 
-WordTable::WordTable() : table_({"null"})
+WordTable::WordTable() : table_({""})
 {
 }
 
@@ -22,28 +22,28 @@ void WordTable::printAll()
     }
 }
 
-void WordTable::add(const std::string &word)
+void WordTable::add(const std::string& word)
 {
     if (std::find(table_.begin(), table_.end(), word) == table_.end())
         table_.push_back(word);
 
 }
 
-void WordTable::add(std::unordered_set<std::string> set)
+void WordTable::add(const std::unordered_set<std::string>& words)
 {
-    for (const auto &elemt: set)
-    {
-        add(elemt);
-    }
-
+    for (const auto& word: words)
+        add(word);
 }
 
-std::string &WordTable::get(uint32_t index)
+const std::string& WordTable::get(uint32_t index)
 {
-    return table_[index];
+    if (index > 0 & index < table_.size())
+        return table_[index];
+    else
+        return table_[0];
 }
 
-uint32_t WordTable::get(const std::string &word)
+uint32_t WordTable::get(const std::string& word)
 {
     auto iter = std::find(table_.begin(), table_.end(), word);
     if (iter == table_.end())
@@ -51,24 +51,27 @@ uint32_t WordTable::get(const std::string &word)
         add(word);
         return table_.size() - 1;
     } else
-    {
         return std::distance(table_.begin(), iter);
-    }
 }
 
-const std::string &WordTable::operator[](uint32_t index) const
+const std::string& WordTable::operator[](uint32_t index)
 {
-    return table_[index];
+    return get(index);
 }
 
-std::string &WordTable::operator[](uint32_t index)
-{
-    return table_[index];
-}
 
-uint32_t WordTable::operator[](std::string word)
+uint32_t WordTable::operator[](const std::string& word)
 {
     return get(word);
+}
+
+uint32_t WordTable::get_const(const std::string& word) const
+{
+    auto iter = std::find(table_.begin(), table_.end(), word);
+    if (iter != table_.end())
+        return std::distance(table_.begin(), iter);
+    else
+        return 0;
 }
 
 } // procedural
