d4ec818799d270eaa444e22dd2ebb782d20e9fb4
avigne <adrien.vigne@laas.fr>
2023-02-09 08:21:21 +0100

    [test] new tes on network to patterntransition

--- src/tests/graph/test_network.cpp
+++ src/tests/graph/test_network.cpp
@@ -1,6 +1,8 @@
 #include "procedural/core/Types/FactPattern.h"
 #include "procedural/graph/Network.h"
 #include <iostream>
+#include <vector>
+#include <set>
 
 int main()
 {
@@ -9,31 +11,56 @@ int main()
     procedural::FactPattern F1 = procedural::FactPattern(false, "A", "hasInHand", "O", false);
     procedural::FactPattern F2 = procedural::FactPattern(true, "O", "overSupport", "S", true);
     procedural::FactPattern F3 = procedural::FactPattern(true, "O", "inContainer", "S", true);
-//    std::cout << "Before Network \n"<< std::endl;
-//    std::cout << F.toString() << std::endl;
-//    std::cout << F1.toString() << std::endl;
-//    std::cout << F2.toString() << std::endl;
-//    std::cout << F3.toString() << std::endl;
-//    std::cout << "END Before Network \n"<< std::endl;
+/*    std::cout << "Before Network \n"<< std::endl;
+   std::cout << F.toString() << std::endl;
+   std::cout << F1.toString() << std::endl;
+   std::cout << F2.toString() << std::endl;
+   std::cout << F3.toString() << std::endl;
+   std::cout << "END Before Network \n"<< std::endl; */
 
-    list_facts.resize(3);
+   /*  list_facts.resize(3);
     list_facts[0].push_back(F);
     list_facts[1].push_back(F1);
     list_facts[2].push_back(F2);
     list_facts[2].push_back(F3);
+ */
+    std::vector<procedural::PatternTransition_t> list_test;
+
+    list_test.emplace_back(0,&F,1,true); 
+    list_test.emplace_back(0,&F1,2,true); 
+    list_test.emplace_back(1,&F1,2,false); 
+    list_test.emplace_back(2,&F2,3,false); 
+    list_test.emplace_back(1,&F2,3,false); 
+    list_test.emplace_back(0,&F2,3,true); 
+
+    procedural::Network N = procedural::Network("pick",0);
+
+    for(const auto& pattern : list_test)
+    {
+        N.addState(pattern);
+    }
+    std::cout << N.map2String()<<"\n" << std::endl;
+    std::cout << N.getCurrentState()->toString() << std::endl;
 
 
-    procedural::Network N = procedural::Network(list_facts, "pick",0);
-    procedural::Network * N2 = N.clone();
+
+
+
+ 
+
+
+
+    // procedural::Network N = procedural::Network(list_facts, "pick",0);
+    // procedural::Network * N2 = N.clone();
 //    N.displayNetwork();
 //    N.displayVariables();
 
     std::vector<procedural::Fact> facts;
     facts.emplace_back(true, "Bastien", "MoveThrought", "Cube");
-    facts.emplace_back(true, "Bob", "hasInHand", "Cube");
-    facts.emplace_back(true, "Cube", "hasInHand", "Cube");
+    // facts.emplace_back(true, "Bob", "hasInHand", "Cube");
+    // facts.emplace_back(true, "Cube", "hasInHand", "Cube");
     facts.emplace_back(true, "Bastien", "hasInHand", "Cube");
-    facts.emplace_back(true, "Cube", "overSupport", "Table");
+    // facts.emplace_back(true, "Cube", "overSupport", "Table");
     facts.emplace_back(false, "Cube", "overSupport", "Table");
 //
     for(auto& fact : facts)
@@ -45,10 +72,11 @@ int main()
 //        N.displayNetwork();
         std::cout << N.getCurrentState()->toString() << std::endl;
     }
-    N.displayNetwork();
+    
     std::cout << "------------------- N2--------------------" << std::endl;
-    N2->displayNetwork();
-
+    // N2->displayNetwork();
+    std::cout << N.map2String()<<"\n" << std::endl;
+ 
 
 
     /*procedural::Fact Fp = procedural::Fact(true, "Bastien", "MoveThrought", "Cube");
