6153b0719275697e4084de527ec9840245710b59
Guillaume Sarthou <gsarthou@laas.fr>
2023-02-13 16:43:28 +0100

    [PatternTransition_t] clean

--- include/procedural/core/Types/PatternTransition.h
+++ include/procedural/core/Types/PatternTransition.h
@@ -5,17 +5,18 @@ namespace procedural
 
 struct PatternTransition_t
 {
-    PatternTransition_t(int origin,FactPattern* fact_,int next,bool initial){
+    PatternTransition_t(int origin, FactPattern* fact, int next, bool initial)
+    {
         origin_state = origin;
-        fact = fact_;
+        this->fact = fact;
         next_state = next;
         is_initial_state = initial;
     }
+
     bool is_initial_state;
     int origin_state;
     FactPattern* fact;
     int next_state;
-
 };
 
 
