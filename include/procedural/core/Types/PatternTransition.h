b86e98de5d01e9911579190cb18094c56c02fe3f
avigne <adrien.vigne@laas.fr>
2023-02-13 18:52:44 +0100

    [PatternTransition] remove initial state

--- include/procedural/core/Types/PatternTransition.h
+++ include/procedural/core/Types/PatternTransition.h
@@ -5,15 +5,13 @@ namespace procedural
 
 struct PatternTransition_t
 {
-    PatternTransition_t(int origin, FactPattern* fact, int next, bool initial)
+    PatternTransition_t(int origin, FactPattern* fact, int next)
     {
         origin_state = origin;
         this->fact = fact;
         next_state = next;
-        is_initial_state = initial;
     }
 
-    bool is_initial_state;
     int origin_state;
     FactPattern* fact;
     int next_state;
