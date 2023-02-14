0c3d4e7237361e0a4cbf8ae7d3c087a583079f6f
avigne <adrien.vigne@laas.fr>
2023-02-14 13:52:36 +0100

    [PatternRecognition] add manage network

--- include/procedural/core/Types/PatternRecognition.h
+++ include/procedural/core/Types/PatternRecognition.h
@@ -2,14 +2,80 @@
 #define PROCEDURAL_PATTERNRECOGNITION_H
 
 #include <vector>
-#include "procedural/core/Types/FactPattern.h"
+#include "procedural/core/Types/PatternTransition.h"
+#include "procedural/core/Graph/Network.h"
+#include "procedural/core/Types/Fact.h"
 
 namespace procedural {
 
 struct PatternRecognition_t
 {
-    std::vector<std::vector<FactPattern>> patterns;
+    
+    PatternRecognition_t(const std::string& name_in,std::vector<procedural::PatternTransition_t>& patterns_in,std::vector<std::string>& descriptions_in):patterns(patterns_in),name(name_in),descriptions(descriptions_in)
+    {    
+    };
+    std::string name;
+    std::vector<procedural::PatternTransition_t>  patterns;
+    Network* root_Network; //issue when i try without *
+    std::unordered_set<Network *> networks_;
+    std::unordered_set<Network *> complete_networks;
     std::vector<std::string> descriptions;
+
+    bool buildNetwork()
+    {
+        root_Network = new Network(name,0);
+        for(auto&  pattern : patterns)
+            root_Network->addTransition(pattern);
+        return root_Network->closeNetwork();
+    }
+
+    int getNextId()
+    {   
+        // random + check on onto if already done 
+        return 10;
+    }
+    void checkNetwork()
+    {
+        for(auto& net : networks_)
+        {
+            checkNetworkComplete(net);
+        }
+    }
+
+    void checkNetworkComplete(Network * net)
+    {
+        if(net->isComplete())
+        {
+            complete_networks.insert(net);
+            networks_.erase(net);
+        }
+    }
+
+    void feed(const Fact& fact)
+    {
+        bool evolve = false;
+        for(auto& net : networks_)
+        {
+            if(net->evolve(fact));
+            {
+                evolve = true;
+                // checkNetworkComplete(net);
+            }
+        }
+        if(not evolve)
+        {
+            Network* new_net = root_Network->clone(getNextId());
+            if(new_net->evolve(fact))
+            {
+                networks_.insert(new_net);
+                // checkNetworkComplete(new_net);
+            }                
+        }
+
+    }
+
+
+     
 };
 
 } // namespace procedural
