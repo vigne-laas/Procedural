67454e0e5b7b397187d6f6d178070dda467ca6e7
avigne <adrien.vigne@laas.fr>
2023-02-24 13:28:30 +0100

    [test] Action add descriptions and verif on complete

--- src/tests/test_action.cpp
+++ src/tests/test_action.cpp
@@ -11,12 +11,21 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "test_action");
     std::vector<procedural::PatternRecognition> list_pattern;
     // procedural::PatternRecognition_t pattern;
-    std::vector<std::string> description1 {"hello pick1","pick1","1"};
-    std::vector<std::string> description2 {"hello pick2","pick2","2"};
+    std::vector<procedural::ActionDescription_t> over_descriptions;
+    over_descriptions.emplace_back("??","isA","PlaceActionOver");
+    over_descriptions.emplace_back("??","isPerformedBy","?A");
+    over_descriptions.emplace_back("??","isPerformedOn","?O");
+    over_descriptions.emplace_back("??","isPerformedFrom","?S");
+    std::vector<procedural::ActionDescription_t> in_descriptions;
+    in_descriptions.emplace_back("??","isA","PlaceActionInto");
+    in_descriptions.emplace_back("??","isPerformedBy","?A");
+    in_descriptions.emplace_back("??","isPerformedOn","?O");
+    in_descriptions.emplace_back("??","isPerformedFrom","?C");
+ 
     procedural::FactPattern F = procedural::FactPattern(false, "A", "MoveThrought", "O", false);
     procedural::FactPattern F1 = procedural::FactPattern(false, "A", "hasInHand", "O", false);
     procedural::FactPattern F2 = procedural::FactPattern(true, "O", "overSupport", "S", true);
-    procedural::FactPattern F3 = procedural::FactPattern(true, "O", "inContainer", "S", true);
+    procedural::FactPattern F3 = procedural::FactPattern(true, "O", "inContainer", "C", true);
 
     std::vector<procedural::PatternTransition_t> list_pick1;
     list_pick1.emplace_back(0, &F, 1); 
@@ -34,8 +43,8 @@ int main(int argc, char **argv)
     list_pick2.emplace_back(1, &F3, 3); 
     list_pick2.emplace_back(0, &F3, 3); 
 
-    list_pattern.emplace_back("pick_in", list_pick1, description1);
-    list_pattern.emplace_back("pick_over", list_pick2, description2);
+    list_pattern.emplace_back("pick_over", list_pick1, over_descriptions);
+    list_pattern.emplace_back("pick_in", list_pick2, in_descriptions);
 
     procedural::Action A("pick");
     for(auto& pattern : list_pattern)
@@ -47,12 +56,13 @@ int main(int argc, char **argv)
 
     std::vector<procedural::Fact> facts;
     facts.emplace_back(true, "Bastien", "MoveThrought", "Cube",1);
-    facts.emplace_back(true, "Bastien", "hasInHand", "Cube",2);
-    facts.emplace_back(false, "Cube", "overSupport", "Table",3);
+   
      facts.emplace_back(true, "Bob", "hasInHand", "Cube2",4);
-    // facts.emplace_back(true, "Cube", "hasInHand", "Cube",5);
-    // facts.emplace_back(true, "Cube", "overSupport", "Table",6);
-    facts.emplace_back(false, "Cube2", "overSupport", "Table",7);
+    facts.emplace_back(true, "Charly", "hasInHand", "Cube",5);
+    facts.emplace_back(true, "Cube", "overSupport", "Table",6);
+    // facts.emplace_back(false, "Cube2", "overSupport", "Table",7);
+     facts.emplace_back(true, "Bastien", "hasInHand", "Cube",2);
+    facts.emplace_back(false, "Cube", "overSupport", "Table",3);
 
     for (auto& fact:facts)
     {
@@ -64,6 +74,9 @@ int main(int argc, char **argv)
     }
     A.displayCurrentState();
     A.checkCompleteNetworks();
+
+    std::cout << A.toString() << std::endl;
+
     ros::shutdown();
 
     return 0;
