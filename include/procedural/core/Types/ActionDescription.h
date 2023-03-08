929bc009e7459e200a1fce83bb3c002063050d4b
avigne <adrien.vigne@laas.fr>
2023-03-08 09:24:04 +0100

    [ActionDescription] clean

--- include/procedural/core/Types/ActionDescription.h
+++ include/procedural/core/Types/ActionDescription.h
@@ -4,37 +4,36 @@
 #include <map>
 #include "procedural/core/Types/Variable.h"
 
-namespace procedural
-{
+namespace procedural {
 struct ActionDescription_t
 {
-    ActionDescription_t(std::string subject,std::string property,std::string object):subject_is_var_(false),object_is_var_(false)
+    ActionDescription_t(std::string subject, std::string property, std::string object) : subject_is_var_(false),
+                                                                                         object_is_var_(false)
     {
         subject_str_ = subject;
         object_str_ = object;
         property_ = property;
         verifVar();
     }
-    
+
     void verifVar()
     {
-        if(subject_str_[0] == '?')
+        if (subject_str_[0] == '?')
         {
-            if(subject_str_.length()==2 && subject_str_[1]=='?')
+            if (subject_str_.length() == 2 && subject_str_[1] == '?')
             {
                 subject_str_ = "self";
                 // subject_is_var_ = true;
-            }
-            else
+            } else
             {
                 subject_is_var_ = true;
                 subject_str_ = subject_str_.substr(1);
             }
-            
+
         }
-        if(object_str_[0] == '?')
+        if (object_str_[0] == '?')
         {
-            if(object_str_.length()==2 && object_str_[1]=='?')
+            if (object_str_.length() == 2 && object_str_[1] == '?')
                 object_str_ = "self";
             else
             {
@@ -49,23 +48,23 @@ struct ActionDescription_t
     std::string toString()
     {
         std::string res = "[Description Action]";
-        res += subject_str_+"|";
-        res += property_+"|";
+        res += subject_str_ + "|";
+        res += property_ + "|";
         res += object_str_;
         return res;
-        
+
     }
 
-    std::string subject_str_ ;
+    std::string subject_str_;
     std::string object_str_;
 
     bool subject_is_var_;
     bool object_is_var_;
 
 
-    std::string property_;    
+    std::string property_;
 };
-    
+
 } // namespace procedural
 
 #endif // PROCEDURAL_ACTIONDESCRIPTION_H
