966ad08c05871015579d0f96cef7fae1f480d6a5
avigne <adrien.vigne@laas.fr>
2023-03-08 09:25:18 +0100

    [Fact] clean

--- src/core/Types/Fact.cpp
+++ src/core/Types/Fact.cpp
@@ -7,7 +7,8 @@ namespace procedural {
 WordTable Fact::properties_table;
 WordTable Fact::individuals_table;
 
-Fact::Fact(bool add, const std::string& subject, const std::string& property, const std::string& object,uint32_t id) : add_(add),id_(id)
+Fact::Fact(bool add, const std::string& subject, const std::string& property, const std::string& object, uint32_t id)
+        : add_(add), id_(id)
 {
     property_ = properties_table.getConst(property);
     subject_ = individuals_table.get(subject);
@@ -16,16 +17,16 @@ Fact::Fact(bool add, const std::string& subject, const std::string& property, co
 
 bool Fact::operator==(const Fact& other) const
 {
-    return (add_ == other.add_) && (property_ == other.property_) && 
+    return (add_ == other.add_) && (property_ == other.property_) &&
            (subject_ == other.subject_) && (object_ == other.object_);
 }
 
 std::string Fact::toString() const
 {
     if (this->isValid())
-        return ((this->add_) ? "ADD " : "DEL " ) + individuals_table[this->subject_] + " " +
-                                       properties_table[this->property_] + " " +
-                                       individuals_table[this->object_];
+        return ((this->add_) ? "ADD " : "DEL ") + individuals_table[this->subject_] + " " +
+               properties_table[this->property_] + " " +
+               individuals_table[this->object_];
     else
         return "";
 }
