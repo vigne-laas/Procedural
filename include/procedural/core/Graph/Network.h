6d4927be8e47ebdfc973b9d7d87943dea194b264
Guillaume Sarthou <gsarthou@laas.fr>
2023-03-13 15:04:36 +0100

    [Network] fix NetworkException use

--- include/procedural/core/Graph/Network.h
+++ include/procedural/core/Graph/Network.h
@@ -14,14 +14,11 @@
 #include "procedural/core/Types/Description.h"
 #include "procedural/core/Types/ActionDescription.h"
 
-
 namespace procedural {
-
 
 struct NetworkException : public std::exception
 {
     std::string msg_;
-    NetworkException(){}; // Pb de compilation et link sans ca
     explicit NetworkException(const std::string& msg) : msg_(msg){}
     const char* what() const throw()
     {
@@ -31,25 +28,16 @@ struct NetworkException : public std::exception
 
 struct NoInitialStateNetworkException : public NetworkException
 {
-    NoInitialStateNetworkException(){}; // Pb de compilation et link sans ca
-    const char* what() const throw()
-    {
-        return "Invalid Network due to no initial State detected";
-    }
+    NoInitialStateNetworkException() : NetworkException("Invalid Network due to no initial State detected"){};
 };
 
 struct MultiInitialStateNetworkException : public NetworkException
 {
-    std::string msg_ = "Invalid Network due to no initial State detected\n";
-    explicit MultiInitialStateNetworkException(const std::unordered_set<State*>& invalid_states)
+    explicit MultiInitialStateNetworkException(const std::unordered_set<State*>& invalid_states) :
+             NetworkException("Invalid Network due to no initial State detected\nState detected as initial are : ")
     {
-        msg_ += " State detected as initial are : ";
         for (auto& state: invalid_states)
             msg_ += state->toString() + "\n";
-    }
-    const char* what() const throw()
-    {
-        return msg_.c_str();
     }
 };
 
