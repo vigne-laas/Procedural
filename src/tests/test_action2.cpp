4fb833be002d86ca6578c0001c58c55079697faa
avigne <adrien.vigne@laas.fr>
2023-04-24 11:44:22 +0200

    [test] exemple of test to trigger timeout completion on release

--- src/tests/test_action2.cpp
+++ src/tests/test_action2.cpp
@@ -7,10 +7,11 @@
 #include "ros/ros.h"
 
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv)
+{
     ros::init(argc, argv, "test_action");
 
-    std::vector<procedural::Action *> Actions_;
+    std::vector<procedural::Action*> Actions_;
     std::cout << "============================ Create data for grasp =============================" << std::endl;
 
 
@@ -24,7 +25,7 @@ int main(int argc, char **argv) {
     grasp_descriptions.emplace_back("??", "isA", "graspAction");
     grasp_descriptions.emplace_back("??", "isPerformedBy", "?A");
     grasp_descriptions.emplace_back("??", "isPerformedOn", "?O");
-    procedural::SpecializedAction pattern_grasp("grasp", list_grasp_network, {}, grasp_descriptions, 20);
+    procedural::SpecializedAction pattern_grasp("grasp", list_grasp_network, {}, grasp_descriptions, 2, 20);
 
     procedural::Action grasp("grasp");
     grasp.addPatterns(pattern_grasp);
@@ -43,7 +44,7 @@ int main(int argc, char **argv) {
     release_descriptions.emplace_back("??", "isA", "release");
     release_descriptions.emplace_back("??", "isPerformedBy", "?A");
     release_descriptions.emplace_back("??", "isPerformedOn", "?O");
-    procedural::SpecializedAction pattern_release("release", release_network, {}, release_descriptions, 20);
+    procedural::SpecializedAction pattern_release("release", release_network, {}, release_descriptions, 2, 20);
 
     procedural::Action release("release");
     release.addPatterns(pattern_release);
@@ -83,11 +84,11 @@ int main(int argc, char **argv) {
     list_pick2_into.emplace_back(0, &F5, 2);
 
     std::vector<procedural::SpecializedAction> list_pattern_pick;
-    list_pattern_pick.emplace_back("pick_over", list_pick_over, patterns_net, over_descriptions, 10);
-    list_pattern_pick.emplace_back("pick_in", list_pick2_into, patterns_net, in_descriptions, 10);
+    list_pattern_pick.emplace_back("pick_over", list_pick_over, patterns_net, over_descriptions, 2, 15);
+    list_pattern_pick.emplace_back("pick_in", list_pick2_into, patterns_net, in_descriptions, 2, 15);
 
     procedural::Action Apick("pick");
-    for (auto &pattern: list_pattern_pick)
+    for (auto& pattern: list_pattern_pick)
         Apick.addPatterns(pattern);
 
     Actions_.push_back(&Apick);
@@ -124,11 +125,12 @@ int main(int argc, char **argv) {
 //    list_pick2_into.emplace_back(0, &F7, 2);
 
     std::vector<procedural::SpecializedAction> list_pattern_place;
-    list_pattern_place.emplace_back("place_over", list_place_over, patterns_net_release, place_over_descriptions, 10);
-    list_pattern_place.emplace_back("place_in", list_place_into, patterns_net_release, place_in_descriptions, 10);
+    list_pattern_place.emplace_back("place_over", list_place_over, patterns_net_release, place_over_descriptions, 1,
+                                    15);
+    list_pattern_place.emplace_back("place_in", list_place_into, patterns_net_release, place_in_descriptions, 1, 15);
 
     procedural::Action Aplace("place");
-    for (auto &pattern: list_pattern_place)
+    for (auto& pattern: list_pattern_place)
         Aplace.addPatterns(pattern);
 
     Actions_.push_back(&Aplace);
@@ -157,7 +159,7 @@ int main(int argc, char **argv) {
     std::vector<procedural::PatternTransitionFact_t> list_pick_place;
     std::vector<procedural::SpecializedAction> list_pattern_pick_place;
     procedural::SpecializedAction pattern_pick_place("pick&place", list_pick_place, patterns_net_pick_place,
-                                                     pick_place_descriptions, 10);
+                                                     pick_place_descriptions, 2, 15);
 //    list_pattern_pick_place.emplace_back("pick&place", list_pick_place, patterns_net_pick_place, pick_place_descriptions, 20);
 
 
@@ -168,7 +170,7 @@ int main(int argc, char **argv) {
 
     Actions_.push_back(&Apick_place);
     std::cout << "======================= Display action : ==========================================" << std::endl;
-    for (auto &action: Actions_)
+    for (auto& action: Actions_)
         std::cout << action->toString() << std::endl;
     std::cout
             << "======================= Feed actions pick with 2 networks type: =========================================="
@@ -177,37 +179,51 @@ int main(int argc, char **argv) {
     procedural::ActionRecognition recognition(Actions_);
 
     std::vector<procedural::Fact> facts;
-    procedural::TimeStamp_t t0(0,50);
-    facts.emplace_back(true, "Bastien", "MoveThrought", "Cube", 1,t0);
+//    procedural::TimeStamp_t t0(0, 50);
+//    facts.emplace_back(true, "Bastien", "MoveThrought", "Cube", 1, t0);
 //    procedural::TimeStamp_t t1(15,0);
 //    facts.emplace_back(true, "Bastien", "MoveThrought", "Cube", 1,t1);
 
 //    procedural::TimeStamp_t t2(20,0);
 //    facts.emplace_back(true, "Bastien", "hasInHand", "Cube", 2,t2);
-    procedural::TimeStamp_t t3(25, 0);
-    facts.emplace_back(false, "Cube", "overSupport", "Table", 3, t3);
-    procedural::TimeStamp_t t4(5,0);
-    facts.emplace_back(true, "Bob", "hasInHand", "Cube2", 4,t4);
+//    procedural::TimeStamp_t t3(25, 0);
+//    facts.emplace_back(false, "Cube", "overSupport", "Table", 3, t3);
+//    procedural::TimeStamp_t t4(5, 0);
+//    facts.emplace_back(true, "Bob", "hasInHand", "Cube2", 4, t4);
 //    facts.emplace_back(true, "Charly", "hasInHand", "Cube", 5);
-    procedural::TimeStamp_t t6(25, 0);
+/*** Pour tester le timeout de release  t6 t7p et 2 boucle de process***/
+    procedural::TimeStamp_t t6(30, 0);
     facts.emplace_back(true, "Cube", "overSupport", "Armoire", 6, t6);
-    procedural::TimeStamp_t t7(28, 0);
-    facts.emplace_back(false, "Bastien", "hasInHand", "Cube", 7, t7);
+    procedural::TimeStamp_t t7p(40, 0);
+    facts.emplace_back(true, "Bastien", "MoveThrought", "Armoire", 6, t7p);
+//    procedural::TimeStamp_t t7(28, 0);
+//    facts.emplace_back(false, "Bastien", "hasInHand", "Cube", 7, t7);
 //     facts.emplace_back(false, "Cube2", "bruit", "Table",7);
 
-    procedural::TimeStamp_t current_time(28,900);
-    for (auto &fact: facts) {
+    procedural::TimeStamp_t current_time(45, 0);
+    for (auto& fact: facts)
+    {
         recognition.addToQueue(&fact);
     }
     recognition.processQueue(current_time);
     std::cout << "\n\n\n\n" << std::endl;
-    for (auto &action: Actions_) {
-//        action->checkCompleteNetworks();
+    procedural::TimeStamp_t current_time2(46, 0);
+    std::cout << "process time : " << current_time2 <<std::endl;
+
+//    for (auto& action: Actions_)
+//    {
+////        action->checkCompleteNetworks();
+//        std::cout << action->toString() << std::endl;
+////        std::cout << action->checkCompleteNetworks() << std::endl;
+////        action->checkCompleteNetworks();
+//    }
+
+    recognition.processQueue(current_time2);
+
+    for (auto& action: Actions_)
         std::cout << action->toString() << std::endl;
-//        std::cout << action->checkCompleteNetworks() << std::endl;
-//        action->checkCompleteNetworks();
-    }
-//    std::cout << Apick.toString() << std::endl;
+
+
 
 
 
