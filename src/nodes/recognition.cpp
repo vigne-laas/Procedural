3839c472160b2d7b820b3ffe147f2ba22efb0992
avigne <adrien.vigne@laas.fr>
2023-06-26 10:32:42 +0200

    [recognition] get name on args

--- src/nodes/recognition.cpp
+++ src/nodes/recognition.cpp
@@ -6,7 +6,24 @@ int main(int argc, char** argv)
     std::cout << "<<<<<<<<<<<<<<<<<< Launching Action Recognition <<<<<<<<<<<<<<<<<<<<<<<<<<<" << std::endl;
     ros::init(argc, argv, "ActionRecognition");
     ros::NodeHandle n;
-    procedural::RosInterface ros_interface(&n);
+    bool var_name = false;
+    std::string name;
+    if (argc > 1)
+        for (int i = 0; i < argc; i++)
+        {
+
+            std::string arg(argv[i]);
+            if (var_name)
+                name = arg;
+            if (arg.find("-n") != std::string::npos)
+                var_name = true;
+            else
+                var_name = false;
+
+
+        }
+
+    procedural::RosInterface ros_interface(&n, name);
     ros_interface.run();
     return 0;
 }
