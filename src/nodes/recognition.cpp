58d41c11cc8db24362d7fd6180f3f4b76f71e583
avigne <adrien.vigne@laas.fr>
2023-06-28 08:58:33 +0200

    [recognition] adapt to Manipulators

--- src/nodes/recognition.cpp
+++ src/nodes/recognition.cpp
@@ -6,12 +6,13 @@ int main(int argc, char** argv)
     std::cout << "<<<<<<<<<<<<<<<<<< Launching Action Recognition <<<<<<<<<<<<<<<<<<<<<<<<<<<" << std::endl;
     ros::init(argc, argv, "ActionRecognition");
     ros::NodeHandle n;
+    auto onto_manipulators = OntologiesManipulator(&n);
+    auto time_manipulators = mementar::TimelinesManipulator(&n);
     bool var_name = false;
     std::string name;
     if (argc > 1)
         for (int i = 0; i < argc; i++)
         {
-
             std::string arg(argv[i]);
             if (var_name)
                 name = arg;
@@ -19,11 +20,9 @@ int main(int argc, char** argv)
                 var_name = true;
             else
                 var_name = false;
+     }
 
-
-        }
-
-    procedural::RosInterface ros_interface(&n, name);
+    procedural::RosInterface ros_interface(&n, onto_manipulators, time_manipulators, name);
     ros_interface.run();
     return 0;
 }
