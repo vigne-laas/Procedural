08eba534c7bee33887a4ffe6205e2e3910201070
Guillaume Sarthou <gsarthou@laas.fr>
2023-07-20 18:09:28 +0200

    [feeder] clean

--- include/procedural/feeder/Feeder.h
+++ include/procedural/feeder/Feeder.h
@@ -13,18 +13,17 @@ class Feeder
 public:
     Feeder();
     void feed(const std::string& fact, int id, TimeStamp_t fact_timstamp);
-    void feed(bool added, const std::string& subject, const std::string& property, const std::string& object, int id,
-              TimeStamp_t fact_timstamp);
-    void setCallback(const std::function<void(Fact*)>& new_callback)
-    { callback_buffer = new_callback; };
+    void feed(bool added, const std::string& subject, const std::string& property, const std::string& object,
+              int id, TimeStamp_t fact_timstamp);
+
+    void setCallback(const std::function<void(Fact*)>& new_callback) { callback_buffer = new_callback; };
 
 private:
-    Fact* parse(const std::string& fact, int id, TimeStamp_t fact_timestamp);
-    static void default_callback(Fact*);
     std::function<void(Fact*)> callback_buffer;
     std::regex const pattern;
 
-
+    Fact* parse(const std::string& fact, int id, TimeStamp_t fact_timestamp);
+    static void default_callback(Fact* fact);
 };
 
 } // procedural
