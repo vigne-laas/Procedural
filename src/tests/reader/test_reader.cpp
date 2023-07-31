782be3a9475290db3aef0913ba03f4ab97020a59
avigne <adrien.vigne@laas.fr>
2023-07-31 10:08:41 +0200

    [Clean] clean + adap to refactor of ActionBuilder

--- src/tests/reader/test_reader.cpp
+++ src/tests/reader/test_reader.cpp
@@ -2,6 +2,7 @@
 #include "procedural/reader/YamlReader.h"
 #include "procedural/builder/ActionBuilder.h"
 #include "procedural/core/ActionRecognition.h"
+
 int main()
 {
     procedural::YamlReader reader = procedural::YamlReader();
