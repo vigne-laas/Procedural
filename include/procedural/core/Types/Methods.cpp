f9073622d6b96c6cf2c8886c528a5e0dab8f287e
avigne <adrien.vigne@laas.fr>
2023-07-31 10:12:51 +0200

    [HTN Types] add Methods + task types to follow htn syntax

---
+++ include/procedural/core/Types/Methods.cpp
@@ -0,0 +1,8 @@
+//
+// Created by avigne on 13/07/23.
+//
+
+#include "Methods.h"
+
+namespace procedural {
+} // procedural
---
+++ include/procedural/core/Types/Methods.h
@@ -0,0 +1,17 @@
+//
+// Created by avigne on 13/07/23.
+//
+
+#ifndef _METHODS_H
+#define _METHODS_H
+
+namespace procedural {
+
+class Methods
+{
+
+};
+
+} // procedural
+
+#endif //_METHODS_H
