d68418f22257458f801f577a7d2ef55951ee2955
avigne <adrien.vigne@laas.fr>
2023-08-10 09:07:42 +0200

    [Types for HTN] method,task,parsedHTN

--- include/procedural/core/Types/Task.h
+++ include/procedural/core/Types/Task.h
@@ -1,12 +1,40 @@
-
 #ifndef PROCEDURAL_TASK_H
 #define PROCEDURAL_TASK_H
+
+#include <string>
+
+#include "ontologenius/API/ontologenius/OntologyManipulator.h"
+
+#include "procedural/core/Types/Action.h"
+#include "procedural/core/Types/Method.h"
+
 
 namespace procedural {
 
 class Task
 {
+public:
+    explicit Task(std::string name) : name_(name), id_(task_types.get(name)){};
 
+    void addMethods(const Method& method);
+    void addArguments(const std::map<std::string, std::string>& arguments);
+
+    bool feed(Action* action);
+    bool feed(Task* task);
+
+    std::string getName() { return name_; }
+    std::vector<Method> getMethods() { return methods_; };
+    uint32_t getId() { return id_; };
+
+    static WordTable task_types;
+private:
+    uint32_t id_;
+    std::map<std::string, std::string> arguments_;
+    std::string name_;
+    std::vector<Method> methods_;
+//    std::vector<>
+
+    std::unordered_set<Task*> complete_task_;
 };
 
 } // procedural
---
+++ src/core/Types/Method.cpp
@@ -0,0 +1,83 @@
+
+#include "procedural/core/Types/Method.h"
+
+namespace procedural {
+Method::Method(const std::string& name, int id) : name_(name), id_(id),compt_SM_(0)
+{
+    factory_machine_ = new StateMachine(name,id);
+}
+
+void Method::addTransition(const HTNTransition_t& transition)
+{
+    factory_machine_->addTransition(transition);
+}
+void Method::close()
+{
+    factory_machine_->closeStateMachine();
+}
+std::string Method::getStrStructure()
+{
+    std::string res;
+    res += "Structure of : " + factory_machine_->getName() + "\n\n";
+    res += factory_machine_->getStrStructure()+"\n";
+    return res;
+}
+bool Method::feed(Action* action)
+{
+    bool evolve = false;
+    for (auto& state_machine: current_state_machines_)
+    {
+        if (state_machine->evolve(action))
+        {
+            std::cout << "\t succes of evolution  : " << state_machine->getName() << std::endl;
+            evolve = true;
+        }
+    }
+
+    if (evolve == false)
+    {
+        StateMachine* new_net = factory_machine_->clone(-1);
+        if (new_net->evolve(action))
+        {
+            new_net->setId(getNextSMId());
+            current_state_machines_.insert(new_net);
+            std::cout << "create new state machine " << new_net->getName() << std::endl;
+
+            evolve = true;
+        }
+        else
+            delete new_net;
+    }
+
+    return evolve;
+}
+bool Method::feed(Task* task)
+{
+    bool evolve = false;
+    for (auto& state_machine: current_state_machines_)
+    {
+        if (state_machine->evolve(task))
+        {
+            std::cout << "\t succes of evolution  : " << state_machine->getName() << std::endl;
+            evolve = true;
+        }
+    }
+
+    if (evolve == false)
+    {
+        StateMachine* new_net = factory_machine_->clone(-1);
+        if (new_net->evolve(task))
+        {
+            new_net->setId(getNextSMId());
+            current_state_machines_.insert(new_net);
+            std::cout << "create new state machine " << new_net->getName() << std::endl;
+
+            evolve = true;
+        }
+        else
+            delete new_net;
+    }
+
+    return evolve;
+}
+} // procedural
