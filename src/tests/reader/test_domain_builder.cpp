9cfec0e75f7f94f424e50e011e030cabd783bde7
avigne <adrien.vigne@laas.fr>
2023-09-04 16:00:19 +0200

    [test] test_domain_builder.cpp update test and add debug

--- src/tests/reader/test_domain_builder.cpp
+++ src/tests/reader/test_domain_builder.cpp
@@ -2,9 +2,11 @@
 #include "procedural/builder/HTNBuilder.h"
 #include "procedural/reader/YamlReader.h"
 #include "procedural/builder/ActionBuilder.h"
+#include "procedural/core/Types/Task.h"
 
 
-int main(int , const char **) {
+int main(int, const char**)
+{
     procedural::DomainReader domainReader;
     procedural::HTNBuilder builder;
     domainReader.getMethods();
@@ -12,8 +14,11 @@ int main(int , const char **) {
     reader.read("/home/avigne/Projets/Procedural/catkin_ws/src/Procedural/src/reader/test_link_domain.yaml");
     procedural::ActionBuilder action_builder(reader.getSimpleActions(), reader.getComposedActions());
     auto actions_ = action_builder.getActions();
-//    std::cout << std::endl;
-    builder.buildTask(domainReader.getMethods(),domainReader.getActions(),action_builder.getActions());
+    std::cout << std::endl;
+    std::cout << "Action Method : \n" << procedural::ActionMethod::action_method_types.toString() << std::endl;
+    std::cout << "Task : \n" << procedural::Task::task_types.toString() << std::endl;
+    std::cout << "Action : \n" << procedural::Action::action_types.toString() << std::endl;
+    builder.buildTask(domainReader.getMethods(), domainReader.getActions(), action_builder.getActions());
     builder.displayActions();
     return 0;
 }
