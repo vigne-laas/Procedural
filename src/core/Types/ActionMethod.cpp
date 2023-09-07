aff149e51c072f131ec130824b2df41fda20e1f8
avigne <adrien.vigne@laas.fr>
2023-09-07 11:45:10 +0200

    [ActionMethod] Observable/Subject version

--- src/core/Types/ActionMethod.cpp
+++ src/core/Types/ActionMethod.cpp
@@ -5,13 +5,15 @@ namespace procedural {
 
 WordTable ActionMethod::action_method_types;
 
-ActionMethod::ActionMethod(const std::string& name) : name_(name), flag_(false),id_(action_method_types.get(name_))
-{}
+ActionMethod::ActionMethod(const std::string& name) : name_(name), flag_(false), id_(action_method_types.get(name_)) {}
 
 bool ActionMethod::addActions(Action* action)
 {
     if (action->isValid())
     {
+        action->attach(this);
+        for(const auto ob:recognition_process_observer_)
+            action->attach(ob);
         actions_.push_back(action);
         return true;
     } else
@@ -22,11 +24,11 @@ bool ActionMethod::feed(Fact* fact, TimeStamp_t currentTimestamp)
 {
     bool evolve = false;
     for (auto& pattern: actions_)
-        if ((currentTimestamp - fact->getTimeStamp()) <= pattern.getTtl())
+        if ((currentTimestamp - fact->getTimeStamp()) <= pattern->getTtl())
             evolve |= pattern->feed(fact);
 //        else
 //        {
-            // std::cout << "rejected fact : " << fact->toString() << " for  :" << pattern.getName() << std::endl;
+    // std::cout << "rejected fact : " << fact->toString() << " for  :" << pattern.getName() << std::endl;
 //            std::cout << "delta _t =" << currentTimestamp - fact->getTimeStamp() << " / ttl : " << pattern.getTtl()
 //                      << std::endl;
 //        }
@@ -44,7 +46,7 @@ std::set<uint32_t> ActionMethod::checkCompleteStateMachines(TimeStamp_t currentT
             set_valid_facts.insert(temp_set.begin(), temp_set.end());
             if (temp_set.empty() == false)
             {
-                std::unordered_set<StateMachine*> temp = pattern.getFinishedStateMachine();
+                std::unordered_set<StateMachine*> temp = pattern->getFinishedStateMachine();
                 complete_state_machines_.insert(temp.begin(), temp.end());
             }
 
@@ -68,7 +70,7 @@ void ActionMethod::clean()
     flag_ = false;
     for (auto& pattern: actions_)
     {
-        pattern.clean();
+        pattern->clean();
     }
     complete_state_machines_.clear();
 }
@@ -122,7 +124,7 @@ std::vector<StateMachine*> ActionMethod::getNewExplanation()
     std::vector<StateMachine*> res;
     for (auto& specializedAction: actions_)
     {
-        auto nets = specializedAction.getUpdatedStateMachines();
+        auto nets = specializedAction->getUpdatedStateMachines();
         res.insert(res.end(), nets.begin(), nets.end());
     }
     return res;
@@ -134,6 +136,35 @@ double ActionMethod::maxTtl()
         if (action->getTtl() > res)
             res = action->getTtl();
     return res;
+}
+void ActionMethod::updateAction(MessageType type, Action* action)
+{
+    std::cout << "receive info from " << action->getName() << " : " << std::to_string((int)type) << std::endl;
+    if (type == MessageType::Finished or type == MessageType::Complete)
+    {
+        finished_actions_.push_back(action);
+
+    }
+    if (type == MessageType::Update)
+        updated_actions_.push_back(action);
+    if (type != MessageType::None)
+    {
+        message_type_ = type;
+        notify();
+    }
+
+}
+void ActionMethod::notify()
+{
+    std::cout << "send data from ActionMethod " << std::to_string((int)message_type_) << std::endl;
+    for (const auto& obs: list_observer_)
+        obs->updateActionMethod(message_type_, this);
+    message_type_ = MessageType::None;
+}
+void ActionMethod::attachRecognitionProcess(IObserver* observer)
+{
+    list_observer_.push_back(observer);
+    recognition_process_observer_.push_back(observer);
 }
 
 
