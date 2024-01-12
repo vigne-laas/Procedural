984bf3ed74966985dbbf5120315afe3ce31264fe
avigne <adrien.vigne@laas.fr>
2024-01-12 14:50:45 +0100

    [Interface] observer/Observable actionEvent add

--- include/procedural/core/Types/Interface.h
+++ include/procedural/core/Types/Interface.h
@@ -1,6 +1,7 @@
 #ifndef PROCEDURAL_INTERFACE_H
 #define PROCEDURAL_INTERFACE_H
 #include <string>
+#include "procedural/core/Types/ResultFeedProcess.h"
 namespace procedural {
 
 class Action;
@@ -23,6 +24,7 @@ public:
     virtual void updateAction(MessageType type, Action* action) {};
     virtual void updateMethod(MessageType type, Method* method) {};
     virtual void updateTask(MessageType type, Task* task) {};
+    virtual void actionEvent(ActionEvent_t event) {};
 };
 
 class ISubject
@@ -31,7 +33,7 @@ public:
     virtual ~ISubject() = default;
     virtual void attach(IObserver* observer) = 0;
     virtual void detach(IObserver* observer) = 0;
-    virtual void notify(MessageType type) = 0;
+    virtual void notify(ActionEvent_t event) = 0;
 };
 }
 
