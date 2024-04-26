6f6bc3cc5c33796079184676abf2523cb038c78f
avigne <adrien.vigne@laas.fr>
2024-04-26 09:13:49 +0200

    [utils] EvolveResult

---
+++ include/procedural/utils/structures/EvolveResult.h
@@ -0,0 +1,12 @@
+#ifndef PROCEDURAL_EVOLVERESULT_H
+#define PROCEDURAL_EVOLVERESULT_H
+namespace recognition{
+enum class EvolveResult
+{
+    NO_EVOLUTION = 0,
+    NEW_EXPLANATION = 1,
+    EVOLVE = 2,
+    FINISH = 3,
+};
+}//namespace procedural
+#endif //PROCEDURAL_EVOLVERESULT_H
