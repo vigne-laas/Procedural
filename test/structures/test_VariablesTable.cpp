06b4aec53d8804bfac43571b4fc91c1ad2b79a79
avigne <adrien.vigne@laas.fr>
2024-04-26 09:17:26 +0200

    [test] add Test on properties link to ontologenius + test VariablesTables

---
+++ test/structures/test_VariablesTable.cpp
@@ -0,0 +1,24 @@
+// test_VariablesTable.cpp
+#include <ros/ros.h>
+#include <gtest/gtest.h>
+#include "procedural/structures/VariablesTable.h"
+#include <memory>
+
+TEST(VariableTableTest, testVariableTable)
+{
+    procedural::VariableTable_t table;
+    procedural::Variable_t var("value");
+    auto var_ptr = std::make_shared<procedural::Variable_t>(var);
+    table.variables["test_var"] = var_ptr;
+    table.agents.insert("test_agent");
+
+    ASSERT_EQ(table.variables["test_var"], var_ptr);
+    ASSERT_NE(table.agents.find("test_agent"), table.agents.end());
+}
+
+int main(int argc, char **argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    ros::init(argc, argv, "test_VariablesTable");
+    return RUN_ALL_TESTS();
+}
