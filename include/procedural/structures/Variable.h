19f7ec3c49818be770530915622c165034357fca
avigne <adrien.vigne@laas.fr>
2024-05-01 17:12:42 +0200

    [variables/variables table] add type on variables + add == on table variables

--- include/procedural/structures/Variable.h
+++ include/procedural/structures/Variable.h
@@ -3,31 +3,52 @@
 
 #include <string>
 #include "ontologenius/clients/ontologyClients/ClassClient.h"
+#include "procedural/utils/Logger.h"
+#include "procedural/utils/WordTable.h"
+
 namespace procedural {
 
 struct Variable_t {
-    explicit Variable_t(const std::string& literal, const std::string& type) : literal_(literal), type_(type),
-                                                                               value_(0) {}
-    Variable_t(const Variable_t& other) = default;
-    Variable_t( Variable_t& other) = default;
+    explicit Variable_t(const std::string& type) : type_(type), value_(0) {}
 
-    Variable_t(const std::string& literal, const std::string& type, onto::ClassClient* class_manipulator) : literal_(
-            literal), type_(type), value_(0)
+    Variable_t(const Variable_t& other) = default;
+
+    Variable_t(Variable_t& other) = default;
+
+    Variable_t(const std::string& type, onto::ClassClient* class_manipulator) : type_(type),
+                                                                                value_(0)
     {
         this->extend(class_manipulator);
     }
 
-    std::string literal_;
+//    void remapTo(std::shared_ptr<Variable_t> remap_var)
+//    {
+//        remaps_ = remap_var;
+//    }
+
+//    bool isRemap() const
+//    {
+//        return remaps_ != nullptr;
+//    }
+
+
+//    std::string literal_;
     std::string type_;
     std::unordered_set<std::string> extended_types_;
     uint32_t value_;
+//    std::shared_ptr<Variable_t> remaps_;
 
 
     bool isSet() const { return value_ != 0; }
 
     uint32_t getValue() const { return value_; }
 
-    void setValue(uint32_t value) { value_ = value; }
+
+    void setValue(uint32_t value)
+    {
+        LOG_DEBUG << "new value : " << value << "\n";
+        value_ = value;
+    }
 
     void extend(onto::ClassClient* class_manipulator)
     {
@@ -41,36 +62,70 @@ struct Variable_t {
 
     std::string getType() const { return type_; }
 
-    std::string toString() const
+    std::unordered_set<std::string> getExtendedTypes() const { return extended_types_; }
+
+    std::string toString(const std::string& literal = "") const
     {
-        std::string str = toShortString();
+
+        std::string str = toShortString(literal);
         str += "(" + type_ + (extended_types_.empty() ? "" : " : ");
         for (const auto& type: extended_types_) {
             str += type + ", ";
         }
         str += ")";
+
         return str;
     }
 
-    std::string toShortString() const { return (value_ ? std::to_string(value_) : literal_); }
+    std::string toShortString(const std::string& literal = "unset") const
+    {
+        return (getValue() ? WordTable::individuals_table[getValue()] + ":" + std::to_string(getValue()) : literal);
+    }
 
     friend std::ostream& operator<<(std::ostream& os, const Variable_t& var) { return os << var.toString(); }
 
     bool operator==(const Variable_t& other) const
     {
+//        LOG_DEBUG << "Compare Variable_t\n";
+//        LOG_DEBUG << toString() << " =? " << other.toString() << "\n";
         if (type_ != other.type_) {
+            if (other.type_.empty() or type_.empty()) {
+                LOG_ERROR << "type_ or other.type_ empty\n";
+                return false;
+            }
+//            LOG_DEBUG << "type_ != other.type_\n";
             std::unordered_set<std::string> intersection;
             std::set_intersection(extended_types_.begin(), extended_types_.end(),
                                   other.extended_types_.begin(), other.extended_types_.end(),
                                   std::inserter(intersection, intersection.begin()));
+            for (const auto& type: intersection) {
+//                LOG_DEBUG << "intersection : " << type << "\n";
+            }
             if (intersection.empty()) {
+//                LOG_DEBUG << "intersection empty\n";
                 return false;
             }
         }
-        if (isSet() and value_ != other.value_)
-            return false;
+//        LOG_DEBUG << "type_ == other.type_\n";
+        if (isSet())
+        {
+//            LOG_DEBUG << "value : " << value_ << " =? " << other.value_ << "\n";
+            return value_ == other.value_;
+        }
         return true;
-        //        return (literal_ == other.literal_) && (value_ == other.value_); }
+    }
+
+
+    bool match(const Variable_t& other)
+    {
+        if(*this == other)
+        {
+
+            if(!isSet())
+                value_ = other.value_;
+            return true;
+        }
+        return false;
     }
 
     bool operator!=(const Variable_t& other) const { return !(*this == other); }
--- include/procedural/structures/VariablesTable.h
+++ include/procedural/structures/VariablesTable.h
@@ -7,28 +7,116 @@
 #include <memory>
 #include "procedural/structures/Variable.h"
 #include "procedural/structures/Fact.h"
+#include "procedural/utils/WordTable.h"
 //#include "procedural/structures/Observation.h"
 
 namespace procedural {
 class Observation;
+
 class ObservationFact;
+
 struct VariableTable_t {
     VariableTable_t() = default;
 
+    VariableTable_t(const VariableTable_t& other)
+    {
+        for (const auto& variable: other.variables)
+            variables[variable.first] = std::make_shared<Variable_t>(*variable.second);
+        agents = other.agents;
+        remap_ = other.remap_;
+    }
+
     std::map<std::string, std::shared_ptr<Variable_t>> variables;
     std::unordered_set<std::string> agents;
-    std::map<std::string,std::string> remap_;
+    std::map<std::string, std::string> remap_;
 
     void set(VariableTable_t other)
     {
+//        LOG_DEBUG << "------------- Set VariableTable ------------------";
+//        LOG_DEBUG << "Origin Graph: " << toString();
+//        LOG_DEBUG << "Other nouvelle transition: " << other.toString();
+
         for (const auto& variable: other.variables) {
-            if (variables.find(variable.first) == variables.end()) {
-                variables[variable.first] = variable.second;
+            if (other.remap_.find(variable.first) != other.remap_.end()) {
+                if (variables.find(other.remap_.at(variable.first)) == variables.end()) {
+//                    LOG_DEBUG << "Set variable " << other.remap_.at(variable.first) << "with remap to "
+//                              << variable.second->toString();
+//                    LOG_DEBUG << "remap from " << variable.first << " to " << other.remap_.at(variable.first);
+                    variables[other.remap_.at(variable.first)] = variable.second;
+                }
+            } else {
+                if (variables.find(variable.first) == variables.end()) {
+//                    LOG_DEBUG << "Set variable " << variable.first << " to " << variable.second->toString();
+                    variables[variable.first] = variable.second;
+                }
+
             }
         }
-        for (const auto& agent: other.agents) {
-            agents.insert(agent);
+        remap_.insert(other.remap_.begin(), other.remap_.end());
+//        LOG_DEBUG << "After set";
+//        LOG_DEBUG << "Graph : " << toString();
+//        LOG_DEBUG << "transition" << other.toString();
+//        LOG_DEBUG << "---------------- END Set VariableTable ------------------";
+
+//        for (const auto& agent: other.agents) {
+//            agents.insert(agent);
+//        }
+    }
+
+    void setRemap(std::map<std::string, std::string> remap_param)
+    {
+        remap_ = remap_param;
+    }
+
+    bool operator==(const VariableTable_t& other) const
+    {
+//        LOG_DEBUG << "Compare VariableTable";
+        if (variables.size() != other.variables.size()) {
+            LOG_DEBUG << "Variables size are different";
+            return false;
         }
+        for (const auto& variable: variables) {
+//            LOG_DEBUG << "Variable : " << variable.first;
+            if (other.variables.find(variable.first) == other.variables.end()) {
+//                LOG_DEBUG << "Variable " << variable.first << " not found in other table";
+                if (remap_.find(variable.first) != remap_.end()) {
+//                    LOG_DEBUG << "Variable " << variable.first << " is remapped to " << remap_.at(variable.first);
+                    if (other.variables.find(remap_.at(variable.first)) == other.variables.end()) {
+                        LOG_ERROR << "Variable " << variable.first << "remap as :" << remap_.at(variable.first)
+                                  << " not found in other table";
+                        return false;
+                    } else {
+                        if (!variable.second->match(*other.variables.at(remap_.at(variable.first)))) {
+//                            LOG_DEBUG << "Variable " << variable.first << "remap as :" << remap_.at(variable.first)
+//                                      << " not equal in other table : "
+//                                      << other.variables.at(remap_.at(variable.first))->toString() << " != "
+//                                      << variable.second->toString();
+                            return false;
+                        }
+//                        else {
+//                            LOG_DEBUG << "Variable " << variable.first << "remap as :" << remap_.at(variable.first)
+//                                      << " equal in other table : "
+//                                      << other.variables.at(remap_.at(variable.first))->toString() << " == "
+//                                      << variable.second->toString();
+//                        }
+                    }
+                } else {
+                    LOG_ERROR << "Variable " << variable.first << " not found in other table even with remap";
+                    return false;
+                }
+            }
+            if (!variable.second->match(*other.variables.at((variable.first)))) {
+                LOG_DEBUG << "Variable " << variable.first << " not equal in other table : "
+                          << other.variables.at(variable.first)->toString() << " != " << variable.second->toString();
+                return false;
+            }
+//            else {
+//                LOG_DEBUG << "Variable " << variable.first << " equal in other table : "
+//                          << other.variables.at(variable.first)->toString() << " == " << variable.second->toString();
+//            }
+
+        }
+        return true;
     }
 
 //    void update(Observation* observation)
@@ -51,9 +139,9 @@ struct VariableTable_t {
         if (!variables.empty()) {
             str += "variables : \n";
             for (const auto& variable: variables) {
-                str += variable.first + " : " + variable.second->toString() +
-                       (variable.second->isSet() ? " : " + Fact::individuals_table[variable.second->value_] : "") +
-                       "\n";
+                str += variable.second->toString(variable.first);
+                str += "[" + std::to_string(reinterpret_cast<uintptr_t>(variable.second.get())) + "]";
+                str += "\n";
             }
         }
         if (!agents.empty()) {
@@ -62,8 +150,7 @@ struct VariableTable_t {
                 str += agent + "\n";
             }
         }
-        if(!remap_.empty())
-        {
+        if (!remap_.empty()) {
             str += "remap : \n";
             for (const auto& remap: remap_) {
                 str += remap.first + " : " + remap.second + "\n";
